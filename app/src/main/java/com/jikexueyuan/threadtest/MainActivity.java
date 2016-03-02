43000f43cdf995a2d4b31e47cd5a1161c8bc450e
Oscar <liu.yang9@hpe.com>
2016-03-02 21:33:49 +0800

    Initial commit

---
+++ app/src/main/java/com/jikexueyuan/threadtest/MainActivity.java
@@ -0,0 +1,68 @@
+package com.jikexueyuan.threadtest;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import java.util.logging.LogRecord;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button changeText;
+    private TextView text;
+    public static final int UPDATE_TEXT = 1;
+
+    private Handler handler = new Handler() {
+
+
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case UPDATE_TEXT:
+                    text.setText("Nice to meet you");
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        changeText = (Button) findViewById(R.id.change_text);
+        text = (TextView) findViewById(R.id.text);
+        changeText.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
+
+    }
+}
